c9c30017dc9e0b5ea5c5f5ecb20ecdb5e8e2e163
S.Samarsky <js.samarsky@gmail.com>
2023-10-14 13:29:11 +0300

    refactor: create component Contacts

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -46,11 +46,9 @@ const Header = () => {
             </div>
           </div>
         </div>
-
-        <div className={styles.header__line}>
-          <div className={styles.line__arrow_left}></div>
-          <div className={styles.line__arrow_right}></div>
-        </div>
+<svg xmlns="http://www.w3.org/2000/svg" width="1216" height="6" viewBox="0 0 1216 6" fill="none">
+<path d="M5 2.5L0 0.113249V5.88675L5 3.5V2.5ZM1211 3.5L1216 5.88675V0.113249L1211 2.5V3.5ZM4.5 3.5H1211.5V2.5H4.5V3.5Z" fill="#524336"/>
+</svg>
 
         <div className={styles.header__menu}>
           <div className={styles.menu__control}>
