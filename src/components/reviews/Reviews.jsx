9864ff6df9ab87e6bf8131ef1c72e67e3fdb3005
S.Samarsky <js.samarsky@gmail.com>
2023-11-15 21:00:07 +0300

    add: responsive layout to 900px for section review

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import BtnOfSlider from "../ui/btns/btnOfSlider/btnOfSlider";
 import Checkbox from "../ui/checkbox/Checkbox";
 import styles from "./Reviews.module.scss";
@@ -5,6 +6,12 @@ import Review from "./review/Review";
 
 
 const Reviews = () => {
+  const [widthWindow, setWidthWindow] = React.useState();
+
+  React.useEffect(() => {
+    window.onresize = () => {setWidthWindow(window.screen.width)};
+    return () => {window.onresize = false};
+}, [widthWindow]);
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Отзывы</h2>
@@ -13,7 +20,7 @@ const Reviews = () => {
         <div className={styles.btn__left}>
           <BtnOfSlider direction={'left'} />
         </div>
-        { Array(2).fill(0).map((el, i) => {
+        { Array(widthWindow > 1450 ? 2 : 1).fill(0).map((el, i) => {
           return <Review key={i} num={i} />
         }) }
         <div className={styles.btn__right}>
