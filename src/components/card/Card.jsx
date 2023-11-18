1f449f9b366861b3ca90e6c2b4d0077f519233d1
S.Samarsky <js.samarsky@gmail.com>
2023-11-18 22:12:14 +0300

    feat: add btn for card

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -2,10 +2,12 @@ import styles from "./Card.module.scss";
 import classNames from "classnames";
 import StatusLabel from "../ui/statusLabel/StatusLabel";
 import Heart from "../ui/icons/heart/Heart";
+import React from "react";
+
 
 const Card = ({ data }) => {
   return (
-    <div className={styles.card}>
+    <div className={classNames(styles.card)}>
       <div
         className={styles.img}
         style={{ backgroundImage: `url(${data.imgUrl})` }}
@@ -25,6 +27,25 @@ const Card = ({ data }) => {
       </div>
       <p className={styles.title}>{data.name}</p>
       <p className={styles.price}>{data.price}</p>
+      <button className={styles.button}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="259"
+          height="50"
+          viewBox="0 0 259 50"
+          fill="none"
+        >
+          <path
+            d="M16 7.5V1H243V7.5V16.25V16.8244L243.496 17.1138L257.015 25L243.496 32.8862L243 33.1756V33.75V42.5V49H16V42.5V33.75V33.1756L15.5039 32.8862L1.98463 25L15.5039 17.1138L16 16.8244V16.25V7.5Z"
+            fill="#EEEEEE"
+            stroke="#BB8C5F"
+            strokeWidth="2"
+          />
+          <text textAnchor="middle" x="130" y="30" style={{ fontSize: "20px" }}>
+            Добавить в корзину
+          </text>
+        </svg>
+      </button>
     </div>
   );
 };
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,40 +1,46 @@
-import Card from '../card/Card';
-import styles from './Products.module.scss';
-import products from '../../assets/data/products.js';
-import Button from '../ui/btns/button/Button';
-import BottomLine from '../ui/bottomLine/BottomLine';
-import classNames from 'classnames';
-
+import Card from "../card/Card";
+import styles from "./Products.module.scss";
+import products from "../../assets/data/products.js";
+import Button from "../ui/btns/button/Button";
+import BottomLine from "../ui/bottomLine/BottomLine";
+import classNames from "classnames";
+import React from "react";
 
 const Products = () => {
-    return (
-        <div className={styles.container}>
-            <ul className={styles.nav}>
-                <li className={styles.nav__item}>
-                    <p className={styles.item__title}>Хиты продаж</p>
-                    <BottomLine />
-                </li>
-                <li className={styles.nav__item}>
-                    <p className={styles.item__title}>Самые популярные</p>
-                    <BottomLine />
-                </li>
-                <li className={classNames(styles.nav__item, styles.item_active)}>
-                    <p className={styles.item__title}>Новые поступления</p>
-                    <BottomLine />
-                </li>
-                <li className={styles.nav__item}>
-                    <p className={styles.item__title}>Акционные товары</p>
-                    <BottomLine />
-                </li>
-            </ul>
-            <div className={styles.cards}>
-                {products.map((el, i) => {
-                    return <Card data={el} key={i} />;
-                })}
-            </div>
-            <Button text={'Показать ещё'}/>
-        </div>
-    );
-}
- 
+
+  return (
+    <div className={styles.container}>
+      <ul className={styles.nav}>
+        <li className={styles.nav__item}>
+          <p className={styles.item__title}>Хиты продаж</p>
+          <BottomLine />
+        </li>
+        <li className={styles.nav__item}>
+          <p className={styles.item__title}>Самые популярные</p>
+          <BottomLine />
+        </li>
+        <li className={classNames(styles.nav__item, styles.item_active)}>
+          <p className={styles.item__title}>Новые поступления</p>
+          <BottomLine />
+        </li>
+        <li className={styles.nav__item}>
+          <p className={styles.item__title}>Акционные товары</p>
+          <BottomLine />
+        </li>
+      </ul>
+      <div className={styles.cards}>
+        {products.map((el, i) => {
+          return (
+            <Card
+              data={el}
+              key={i}
+            />
+          );
+        })}
+      </div>
+      <Button text={"Показать ещё"} />
+    </div>
+  );
+};
+
 export default Products;
